3d89bbcd6b9e46100600aef53884274eea8ee53e
DaniloK-SS <danilodakakovacevic@gmail.com>
2026-03-06 13:21:33 +0100

    napravio sam header sekciju

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-
-import './App.css'
+import { Navbar } from "./components/navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home";
+import Genre from "./routes/Genre";
+import Movies from "./routes/movies";
+import TvShow from "./routes/TvShow";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="min-h-screen bg-linear-to-br from-blue-50 via-blue-100 to-blue-200 text-gray-900 pt-24">
+
+      <Navbar />
+
+      <main className="max-w-7xl mx-auto px-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Genre" element={<Genre />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/TvShow" element={<TvShow />} />
+        </Routes>
+      </main>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/routes/home.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="mt-6 grid gap-6">
+
+      <div className="bg-white/80 backdrop-blur-md p-8 rounded-3xl shadow-lg border border-white/40">
+        <h1 className="text-3xl font-bold text-blue-500 mb-3">
+          Home Page
+        </h1>
+
+        <p className="text-gray-600">
+          Welcome to Flixora streaming platform.
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;

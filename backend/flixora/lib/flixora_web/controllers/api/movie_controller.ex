8552f74dcf628b1d7319781963716f65c1ee5fcb
majaantovic00-coder <majaantovic00@gmail.com>
2026-04-20 14:33:52 +0200

    bduwqhiu

--- backend/flixora/lib/flixora_web/controllers/api/movie_controller.ex
+++ backend/flixora/lib/flixora_web/controllers/api/movie_controller.ex
@@ -3,15 +3,63 @@ defmodule FlixoraWeb.Api.MovieController do
 
   alias Flixora.Movies
 
+
   def index(conn, params) do
     movies = Movies.list_movies(params)
-    render(conn, :index, movies: movies)
+    json(conn, movies)
   end
+
 
   def show(conn, %{"id" => id}) do
     case Movies.get_movie(id) do
       {:ok, movie} ->
-        render(conn, :show, movie: movie)
+        json(conn, movie)
+
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Movie not found"})
+    end
+  end
+
+
+  def create(conn, params) do
+    case Movies.create_movie(params) do
+      {:ok, movie} ->
+        conn
+        |> put_status(:created)
+        |> json(movie)
+
+      {:error, _changeset} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Invalid data"})
+    end
+  end
+
+
+  def update(conn, %{"id" => id} = params) do
+    case Movies.update_movie(id, params) do
+      {:ok, movie} ->
+        json(conn, movie)
+
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Movie not found"})
+
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Update failed"})
+    end
+  end
+
+
+  def delete(conn, %{"id" => id}) do
+    case Movies.delete_movie(id) do
+      {:ok, _} ->
+        send_resp(conn, :no_content, "")
 
       {:error, :not_found} ->
         conn
--- backend/flixora/lib/flixora_web/controllers/api/show_controller.ex
+++ backend/flixora/lib/flixora_web/controllers/api/show_controller.ex
@@ -3,20 +3,65 @@ defmodule FlixoraWeb.Api.ShowController do
 
   alias Flixora.Shows
 
+
   def index(conn, params) do
     shows = Shows.list_shows(params)
-    render(conn, shows: shows)
+    json(conn, shows)
   end
 
   def show(conn, %{"id" => id}) do
     case Shows.get_show(id) do
       {:ok, show} ->
-        render(conn, :show, show: show)
+        json(conn, show)
 
       {:error, :not_found} ->
         conn
         |> put_status(:not_found)
-        |> json(%{error: "Movie not found"})
+        |> json(%{error: "Show not found"})
+    end
+  end
+
+  def create(conn, params) do
+    case Shows.create_show(params) do
+      {:ok, show} ->
+        conn
+        |> put_status(:created)
+        |> json(show)
+
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Invalid data"})
+    end
+  end
+
+  def update(conn, %{"id" => id} = params) do
+    case Shows.update_show(id, params) do
+      {:ok, show} ->
+        json(conn, show)
+
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Show not found"})
+
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Update failed"})
+    end
+  end
+
+
+  def delete(conn, %{"id" => id}) do
+    case Shows.delete_show(id) do
+      {:ok, _} ->
+        send_resp(conn, :no_content, "")
+
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Show not found"})
     end
   end
 end

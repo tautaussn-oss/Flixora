badf6bcf0dc7f6b9970c6dbc6931130e6e46ccf8
Nikola <tau.taussn@gmail.com>
2026-04-23 14:20:00 +0200

    fix attempt

---
+++ backend/flixora/priv/repo/migrations/20260423121512_delete_timestamps_many_to_many_tables.exs
@@ -0,0 +1,25 @@
+defmodule Flixora.Repo.Migrations.DeleteTimestampsManyToManyTables do
+  use Ecto.Migration
+
+  def change do
+    alter table(:movies_actors) do
+      remove :inserted_at
+      remove :updated_at
+    end
+
+    alter table(:shows_actors) do
+      remove :inserted_at
+      remove :updated_at
+    end
+
+    alter table(:movies_genres) do
+      remove :inserted_at
+      remove :updated_at
+    end
+
+    alter table(:shows_genres) do
+      remove :inserted_at
+      remove :updated_at
+    end
+  end
+end

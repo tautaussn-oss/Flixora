2e292537d3322db197f92c4543b33eb8a63cc365
Nikola <tau.taussn@gmail.com>
2026-04-28 13:28:35 +0200

    push

--- backend/flixora/lib/flixora_web/router.ex
+++ backend/flixora/lib/flixora_web/router.ex
@@ -1,6 +1,8 @@
 defmodule FlixoraWeb.Router do
   use FlixoraWeb, :router
 
+  import FlixoraWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule FlixoraWeb.Router do
     plug :put_root_layout, html: {FlixoraWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -27,11 +30,6 @@ defmodule FlixoraWeb.Router do
     resources "/shows", ShowController, except: [:new, :edit]
     resources "/genres", GenreController, only: [:index, :show]
     resources "/actors", ActorController, only: [:index, :show]
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
-    post "/users", UserController, :create
-    put "/users/:id", UserController, :update
-    delete "/users/:id", UserController, :delete
 
     # post "/login", AuthController, :login
   end
@@ -56,5 +54,31 @@ defmodule FlixoraWeb.Router do
       live_dashboard "/dashboard", metrics: FlixoraWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", FlixoraWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+  end
+
+  scope "/", FlixoraWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm-email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", FlixoraWeb do
+    pipe_through [:browser]
+
+    get "/users/log-in", UserSessionController, :new
+    get "/users/log-in/:token", UserSessionController, :confirm
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
